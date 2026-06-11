8ac9ca1a0697fd384b3b8c27cf13ce21879018cd
man <nguyenman28012007@gmail.com>
2026-06-11 16:17:15 +0700

    1162026

--- js/lib.js
+++ js/lib.js
@@ -1,4 +1,81 @@
 /*javascript*/
+//Khai báo một đối tượng
+const product = {
+    id:"1",
+    name:" GÀ QUAY ",
+    price:279000,
+    description:" Gà quay mật ong da giòn...",
+    image:"../assets/images/gaquay.jpg",
+    link:"chi-tiet.html"
+};
+
+const products = [
+    { 
+        id:"1",
+    name:" HAMBURGER ",
+    price:79000,
+    description:"Hamburger đậm vị Ngon trên từng thớ thịt...",
+    image:"../assets/images/hamburger.jpg",
+    link:"chi-tiet.html"
+    },
+    { 
+        id:"2",
+    name:" BÁNH MÌ ",
+    price:25000,
+    description:"Bánh mì heo quay,Da giòn rôm rốp, thịt đầy ngập răng...",
+    image:"../assets/images/banhmi.jpg",
+    link:"chi-tiet.html"
+    },
+    {
+       id:"3",
+    name:"MÌ CAY",
+    price:50000,
+    description:"Sợi mì dai chuẩn Hàn đẫm trong nước dùng đậm đà...",
+    image:"../assets/images/micay.jpg",
+    link:"chi-tiet.html" 
+    },
+
+   {
+    id:"4",
+    name:" GÀ QUAY ",
+    price:279000,
+    description:" Gà quay mật ong da giòn...",
+    image:"../assets/images/gaquay.jpg",
+    link:"chi-tiet.html"
+   }
+];
+
+function addItemV2(product){
+    document.getElementById("product-list").innerHTML += `
+        <div class="col">
+            <div class="card product-item h-100">
+                <div class="product-image">
+                    <img class="card-img-top" src="${product.image}" alt="${product.name}">
+                </div>
+                <div class="card-body bg-light product-info">
+                    <h4 class="card-title">${product.name}</h4>
+                    <h5 class="card-text">${product.price} VNĐ</h5>
+                    <p class="card-text">${product.description}</p>
+                    <a class="btn btn-info" href="${product.link}">Xem chi tiết</a>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function loadAllProduct(){
+    let i = 0;
+    while(i < products.length)
+    {
+        addItemV2(products[i]);
+        i++;
+    }
+}
+
+
+
+
+
 function inBCC(n)
 {
     let result = "";
